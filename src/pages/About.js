edf6d53bf6a65d8ad4b47f48a5e0b9ce4fe216fa
Ninetta11 <ninawelsh1@outlook.com>
2021-01-21 14:46:11 +1100

    Fixed image load issue

--- src/pages/About.js
+++ src/pages/About.js
@@ -8,7 +8,7 @@ function About() {
             <div className="col-md-1"></div>
             <article id="about" className="col-md-6">
                 <h3>About</h3>
-                <img id="about-img" className="right" src={process.env.PUBLIC_URL + 'assets/images/IMG_0006.jpeg'}
+                <img id="about-img" className="right" src={process.env.PUBLIC_URL + '/assets/images/IMG_0006.jpeg'}
                     alt="Nina Welsh Profile Image" width="250" height="250"></img>
                 <p>Full stack web developer with extensive program and project management experience in a
                 diverse range of industries. A Certificate from Sydney University in full stack web
