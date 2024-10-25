2c1c28d673c9e3bc75d353137350e185bff12f6f
Ninetta11 <ninawelsh1@outlook.com>
2024-10-25 17:35:07 +1100

    Update content

--- src/components/Bio/index.js
+++ src/components/Bio/index.js
@@ -6,14 +6,13 @@ function Bio() {
             <div className="row">
                 <div className="col-md-1"></div>
                 <div className="col-md-4">
-                    <img className="centre" src={process.env.PUBLIC_URL + '/assets/images/IMG_0006.jpeg'} alt="Nina Welsh Profile" width="300"
+                    <img className="centre" src={process.env.PUBLIC_URL + '/assets/images/Profile Image.jpg'} alt="Nina Welsh Profile" width="300"
                         height="300"></img>
                 </div>
                 <div className="col-md-6">
-                    <h2 className="d-flex align-items-center">Full Stack Developer, Entrepreneur, Business Developer, Idealist,
-                    Hiker, Intrepid
-                    Traveller, Book Collector
-                </h2>
+                    <h2 className="d-flex align-items-center">Full Stack Developer, Doer, Idealist,
+                        Hiker, Intrepid Traveller, Book Collector, Organic Gardener
+                    </h2>
                 </div>
             </div>
         </article>
--- src/components/Profile/index.js
+++ src/components/Profile/index.js
@@ -3,18 +3,11 @@
 function Profile() {
     return (
         <article id="about" className="col-md-12">
-            <h3>About</h3>
-            <h4 id="about_text">Full Stack Software Engineer with extensive program and project management experience in a
-                diverse range of industries. A Certificate from Sydney University in full stack web
-                technologies, including Javascript, CSS, HTML, Node.JS, Express, React, MySQL, NoSQL and
-                others, as well as a Bachelor Degree in Development Studies from La Trobe University.
-                Previous professional experience includes establishing businesses and programs from concept
-                to execution, including ongoing management and development, across hospitality and retail as
-                well as the social welfare sector. Thrives when able to apply advanced problem solving
-                skills and work autonomously. Seeking to apply this broad range of skills and experience,
-                together with a passion for societal change towards a more sustainable and equitable future,
-                within the blockchain and distributed ledger technology space.</h4>
-        </article>
+            <h3>About me</h3>
+            <h4 id="about_text">I am a Full Stack Software Engineer with experience across diverse industries. I am proficient in Node.js, .NET, JavaScript/TypeScript, C#, React, Angular, Auth0, and Azure, and I have explored and worked with a variety of other languages, frameworks, and technologies. I also recently completed a Bitcoin Protocol Development series with Chaincode Labs.</h4>
+            <h4 id="about_text">My professional and personal journey has also involved establishing businesses and programs from conception to ongoing management, particularly in hospitality, retail, and the social welfare sector. I excel in advanced problem-solving and thrive in autonomous work environments. By nature I love a challenge, and am constantly collecting new skills and experiences.</h4>
+            <h4 id="about_text">I am passionate about leveraging my diverse skills and experiences to drive positive societal change, particularly in the blockchain and distributed ledger technology space. I am eager to contribute to a more sustainable and equitable future through innovative tech solutions.</h4>
+        </article >
     )
 }
 
